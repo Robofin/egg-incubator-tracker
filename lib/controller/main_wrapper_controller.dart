fe699ef949c9d22edfbdd5bcaf8c9a0071728ff4
Robofin <finamsiska98@gmail.com>
2023-10-25 09:32:18 +0200

    egg-incubator-tracker-v1.0

---
+++ lib/controller/main_wrapper_controller.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../view/cart_tab.dart';
+import '../view/home_tab.dart';
+import '../view/profile_tab.dart';
+import '../view/statistics_tab.dart';
+
+class MainWrapperController extends GetxController {
+  late PageController pageController;
+
+  RxInt currentPage = 0.obs;
+  RxBool isDarkTheme = false.obs;
+
+  List<Widget> pages = [
+    const HomeTab(),
+    const CartTab(),
+    const StatisticsTab(),
+    const ProfileTab(),
+  ];
+
+  ThemeMode get theme => Get.isDarkMode ? ThemeMode.dark : ThemeMode.light;
+
+  void switchTheme(ThemeMode mode) {
+    Get.changeThemeMode(mode);
+  }
+
+  void goToTab(int page) {
+    if (page >= 0 && page < pages.length) {
+      currentPage.value = page;
+      pageController.jumpToPage(page);
+    }
+  }
+
+  void animateToTab(int page) {
+    if (page >= 0 && page < pages.length) {
+      currentPage.value = page;
+      pageController.animateToPage(page,
+          duration: const Duration(milliseconds: 100), curve: Curves.ease);
+    }
+  }
+
+  @override
+  void onInit() {
+    pageController = PageController(initialPage: currentPage.value);
+    super.onInit();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+}
---
+++ lib/view/home_tab.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Home',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          const Icon(
+            IconlyLight.home,
+            size: 40,
+          ),
+        ],
+      ),
+    );
+  }
+}
